369f1ace0f45cf981fcab68c7e0fc1cf0a6e04fc
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-02-04 22:28:29 +0900

    Create MatrixMultiplication.c

---
+++ MatrixMultiplication.c
@@ -0,0 +1,12 @@
+int (*MatrixMultiplication(int a[][1000], int b[][1000], int n))[1000] {
+    static int c[1000][1000];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            c[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    return c;
+}
