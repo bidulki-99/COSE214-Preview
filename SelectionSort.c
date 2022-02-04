454f2cc7bfcf5cf64f1698344f586cae31fb4a82
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-02-05 04:22:19 +0900

    Create SelectionSort.c

---
+++ SelectionSort.c
@@ -0,0 +1,14 @@
+void SelectionSort(int a[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        int min = i;
+        for (int j = i+1; j < n; j++)
+            if (a[j] < a[min])
+                min = j;
+        int temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+}
