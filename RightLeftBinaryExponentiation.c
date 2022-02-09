eb5db0c19d3df39da9a9e8224a02273ce9b5d0b0
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-02-09 18:33:37 +0900

    Create RightLeftBinaryExponentiation.c

---
+++ RightLeftBinaryExponentiation.c
@@ -0,0 +1,11 @@
+int RightLeftBinaryExponentiation(int a, char b[]) {
+    int len = strlen(b);
+    int term = a;
+    int product = b[len-1] == '1' ? a : 1;
+    for (int i = len-2; i >= 0; i--) {
+        term *= term;
+        if (b[i] == '1')
+            product *= term;
+    }
+    return product;
+}
