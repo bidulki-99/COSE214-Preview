56a4033e56dd72cf656ebd063874256a881ebedb
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-05-02 14:44:25 +0900

    Update QuickSelect.c

--- QuickSelect.c
+++ QuickSelect.c
@@ -1,9 +1,9 @@
 int QuickSelect(int a[], int l, int r, int k) {
     int s = LomutoPartition(a, l, r);
-    if (s == k-1)
+    if (s == l+k-1)
         return a[s];
     else if (s > l+k-1)
         QuickSelect(a, l, s-1, k);
     else
-        QuickSelect(a, s+1, r, k);
+        QuickSelect(a, s+1, r, l+k-1-s);
 }
